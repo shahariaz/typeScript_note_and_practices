15a846dae61a915c069287a4864bf80d1a7ac0fa
shahariaz <shahariaz.info@gmail.com>
2023-09-04 00:12:51 +0600

    first commit

---
+++ variable.ts
@@ -0,0 +1,8 @@
+//string
+let greetings: string = "hello world";
+//Number type
+// in js there is no int or float type all are just numbers
+let userId: number = 0;
+//boolen type
+
+let isLoggedin: boolean = false;
